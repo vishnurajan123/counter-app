7d93e5ea64a2cb4b201de57690f43845918a61b8
vishnurajan123 <vishnurajan135531@gmail.com>
2023-10-16 12:31:05 +0530

    Counter App

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { decrement, increment, reset } from '../redux/CounterSlice'
+import { decrement, increment, incrementByAmount, reset } from '../redux/CounterSlice'
 
 function Counter() {
+
+  const [amount,setAmount]=useState("")
     const dispatch=useDispatch()
     const count=useSelector((state)=>state.counter.count)
+
+    const handleIncrement=()=>{
+      if(amount==""){
+        alert("please provide amount..!")
+     }
+     else{
+      // dispatch action
+      dispatch(incrementByAmount(Number(amount)))
+     }
+    }
+
   return (
     <div style={{height:"70vh"}} className='w-100 d-flex justify-content-center align-items-center'> 
     <div className='d-flex justify-content-center align-items-center border rounded p-5 w-50 flex-column'>
@@ -13,6 +26,14 @@ function Counter() {
         <button onClick={()=>dispatch(decrement())} className='btn btn-warning'>Decrement</button>
         <button onClick={()=>dispatch(reset())} className='btn btn-danger'>Reset</button>
         <button onClick={()=>dispatch(increment())} className='btn btn-success'>Increment</button>
+    </div>
+    <div className='d-flex m-5 flex-wrap'>
+      
+      <input onChange={(e)=>setAmount(e.target.value)} type="text" className="form-control m-2" placeholder='Enter the amount'/>
+      <button onClick={handleIncrement} 
+      
+      className='btn btn-info ms-3' > Increment by amount</button>
+  
     </div>
     </div>
         
--- src/redux/CounterSlice.js
+++ src/redux/CounterSlice.js
@@ -14,9 +14,12 @@ const counterSlice = createSlice({
         },
         reset:(state)=>{
             state.count=0
+        },
+        incrementByAmount:(state,action)=>{
+            state.count+=action.payload
         }
     }
 })
 
-export const {increment,decrement,reset}=counterSlice.actions
+export const {increment,decrement,reset,incrementByAmount}=counterSlice.actions
 export default counterSlice.reducer
